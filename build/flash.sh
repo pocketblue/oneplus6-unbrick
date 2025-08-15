3148e0cad1583d9a20bb1dfccc60ecdcc60d5e49
gmanka <gmankab@gmail.com>
2025-08-15 15:36:55 +0500

    [u] flashing script

--- build/flash.sh
+++ build/flash.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -ux
 which edl
 
-export file=$(realpath $0)
-export dir=$(dirname $file)
-cd $dir/images
-
-edl qfil rawprogram0.xml patch0.xml .
-edl qfil rawprogram1.xml patch1.xml .
-edl qfil rawprogram2.xml patch2.xml .
-edl qfil rawprogram3.xml patch3.xml .
-edl qfil rawprogram4.xml patch4.xml .
-edl qfil rawprogram5.xml patch5.xml .
+edl qfil rawprogram0.xml patch0.xml images
+edl qfil rawprogram1.xml patch1.xml images
+edl qfil rawprogram2.xml patch2.xml images
+edl qfil rawprogram3.xml patch3.xml images
+edl qfil rawprogram4.xml patch4.xml images
+edl qfil rawprogram5.xml patch5.xml images
