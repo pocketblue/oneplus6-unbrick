f968045135224ee290f6abc0a73892b828015f63
gmanka <gmankab@gmail.com>
2025-08-15 14:47:56 +0500

    [u] flash script

--- build/flash.sh
+++ build/flash.sh
@@ -3,9 +3,13 @@
 set -euo pipefail
 which edl
 
-edl qfil rawprogram0.xml patch0.xml images
-edl qfil rawprogram1.xml patch1.xml images
-edl qfil rawprogram2.xml patch2.xml images
-edl qfil rawprogram3.xml patch3.xml images
-edl qfil rawprogram4.xml patch4.xml images
-edl qfil rawprogram5.xml patch5.xml images
+export file=$(realpath $0)
+export dir=$(dirname $file)
+cd $dir/images
+
+edl qfil rawprogram0.xml patch0.xml .
+edl qfil rawprogram1.xml patch1.xml .
+edl qfil rawprogram2.xml patch2.xml .
+edl qfil rawprogram3.xml patch3.xml .
+edl qfil rawprogram4.xml patch4.xml .
+edl qfil rawprogram5.xml patch5.xml .
